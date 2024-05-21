66f90d23af2e3ad9604c6fc138ed0e8a53a39549
Samuel Oliveira <S.SamuelS.a.m@hotmail.com>
2024-05-20 23:31:06 -0300

    Configuração da inicialização do banco de dados, linha: 20.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+
 namespace SistemaDeTarefas
 {
     public class Program
@@ -13,6 +16,8 @@ namespace SistemaDeTarefas
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddEntityFrameworkSqlServer().AddDbContext<SistemaTarefasDBContex>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 
             var app = builder.Build();
 
